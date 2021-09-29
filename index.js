835ea7e2053f76a3fa5b3caf7a77fa5ed2ee9823
Carlos Mertens <mertenscarlos@yahoo.com>
2021-09-29 23:52:32 +0200

    Setup function to display photos

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unplash API
 const count = 10;
-const apiKey = 'N6GI-vtS-1oIfxYrRYDmqNZplk8uZvlUA972fr2wc4Y';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${config.apiKey}&count=${count}`;
+
+// Create elemnts to disply photos, add them to DOM
+function displayPhotos() {
+  // Loop photosArray
+  photosArray.forEach((photo) => {
+    console.log(photo);
+  });
+}
 
 // Get photos from Unplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {
     // Catch error
   }
